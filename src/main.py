7fb2eca3e911fd1666516d7b6c7158742290b87f
paulwinex <paulwinex@gmail.com>
2021-02-26 13:28:58 +0300

    WIP

--- src/main.py
+++ src/main.py
@@ -10,30 +10,6 @@ DISPLAY_SDA_PIN = const(21)
 RELAY_PIN = const(10)
 
 
-class Display:
-    DEFAULT_I2C_ADDR = 0x27
-    ROWS = 4
-    LINES = 20
-
-    def __init__(self):
-        i2c = I2C(0, scl=Pin(DISPLAY_SCL_PIN), sda=Pin(DISPLAY_SDA_PIN), freq=400000)
-        self.lcd = I2cLcd(i2c, self.DEFAULT_I2C_ADDR, 4, 20)
-
-    def display(self, text):
-        lines = text.split('\n')  # type: list
-        if len(lines) < 4:
-            lines += [''] * (4 - len(lines))
-        lines = ['{:<20}'.format(l[:20]) for l in lines]
-        self.lcd.putstr('\n'.join([x for x in lines]))
-
-    def print_line(self, line, text, start_pos=0):
-        self.lcd.move_to(start_pos, line)
-        self.lcd.putstr(text)
-
-
-LCD = Display()
-
-
 class RObject:
     def __init__(self):
         self.__events = []
@@ -47,6 +23,57 @@ class RObject:
 
     def receive(self, event, *args):
         pass
+
+
+class Display(RObject):
+    DEFAULT_I2C_ADDR = 0x27
+    ROWS = 4
+    LINES = 20
+
+    def __init__(self):
+        super(Display, self).__init__()
+        i2c = I2C(0, scl=Pin(DISPLAY_SCL_PIN), sda=Pin(DISPLAY_SDA_PIN), freq=400000)
+        self.lcd = I2cLcd(i2c, self.DEFAULT_I2C_ADDR, 4, 20)
+        self.render_enabled = True
+        self.timer = None
+
+    def display(self, text):
+        if not self.render_enabled:
+            return
+        lines = text.split('\n')  # type: list
+        if len(lines) < 4:
+            lines += [''] * (4 - len(lines))
+        lines = ['{:<20}'.format(l[:20]) for l in lines]
+        self.lcd.putstr('\n'.join([x for x in lines]))
+
+    def print_line(self, line, text, start_pos=0):
+        if not self.render_enabled:
+            return
+        self.lcd.move_to(start_pos, line)
+        self.lcd.putstr(text)
+
+    def show_message(self, text, title='ERROR', timeout=3):
+        lines = text.split('\n')
+        lines = [x.strip()[:20].center(20) for x in lines]
+        title = title[:20].center(20)
+        self.lcd.clear()
+        self.lcd.move_to(0, 0)
+        self.lcd.putstr(title)
+        for i, line in enumerate(lines[:3], 1):
+            self.lcd.move_to(0, i)
+            self.lcd.putstr(line)
+        self.render_enabled = False
+        self.timer = Timer(2)
+        self.timer.init(period=timeout*1000, mode=Timer.ONE_SHOT, callback=self.hide_message)
+
+    def hide_message(self, *args):
+        self.timer = None
+        self.render_enabled = True
+        self.lcd.clear()
+        self.emit('rerender')
+
+
+LCD = Display()
 
 
 class EncoderEvents(RObject):
@@ -66,22 +93,19 @@ class EncoderEvents(RObject):
         self.op_clb = on_press
 
     def on_left(self):
-        print('left')
         self.ol_clb()
 
     def on_right(self):
-        print('right')
         self.or_clb()
 
     def on_press(self):
-        print('press')
         self.op_clb()
 
     def update(self):
         # encoder
         value = self._enc.getValue()
         if value != self.last_enc_value:
-            if value < self.last_enc_value:
+            if value > self.last_enc_value:
                 self.on_left()
             else:
                 self.on_right()
@@ -94,10 +118,108 @@ class EncoderEvents(RObject):
             self.last_btn_value = btn_value
 
 
+class Program(RObject):
+    STATE_STOPPED = 0
+    STATE_OFFLINE = 1
+    STATE_ONLINE = 2
+
+    def __init__(self):
+        super(Program, self).__init__()
+        self.state = 0
+        self.led = Pin(LED_PIN, Pin.OUT)
+        self.led.value(0)
+        self.rtc = RTC()
+        self.timer = None
+        self.on_time = 0
+        self.off_time = 0
+        self.eta = 0
+        self.set_state(self.STATE_STOPPED)
+
+    def start_timer(self):
+        # https://micronote.tech/2020/02/Timers-and-Interrupts-with-a-NodeMCU-and-MicroPython/
+        # check times
+        if self.state != self.STATE_STOPPED:
+            return
+        if self.timer:
+            self.stop_timer()
+        self.timer = Timer(-2)
+        self.timer.init(period=1000, mode=Timer.PERIODIC, callback=self.update_handler)
+        self.rest_rtc()
+
+    def stop_timer(self, set_state=False):
+        if self.state == self.STATE_STOPPED:
+            return
+        if self.timer:
+            self.timer.deinit()
+            self.timer = None
+        if set_state:
+            self.set_state(self.STATE_STOPPED)
+
+    def set_state(self, state):
+        self.state = state
+        self.emit('set_state', self.state)
+
+    def rest_rtc(self):
+        self.rtc.datetime((2020, 1, 1, 0, 0, 0, 0, 0))
+        self.eta = 0
+
+    def _get_time_offset(self):
+        if not self.timer:
+            return 0
+        _, _, _, _, h, m, s, _ = self.rtc.datetime()
+        return (h * 60 * 60) + (m * 60) + s
+
+    def update_handler(self, *args):
+        # get current time offset
+        total_sec = self._get_time_offset()
+        # compute eta
+        # trigger state if need
+        if self.state == self.STATE_ONLINE:
+            self.eta = self.on_time - total_sec
+            # if total_sec >= self.on_time:
+            if self.eta == 0:
+                self.on_state_triggered(self.STATE_OFFLINE)
+        elif self.state == self.STATE_OFFLINE:
+            self.eta = self.off_time - total_sec
+            # if total_sec >= self.off_time:
+            if self.eta == 0:
+                self.on_state_triggered(self.STATE_ONLINE)
+        self.update_display()
+
+    def update_display(self):
+        self.emit('update_eta', self.eta)
+
+    def receive(self, event, *args):
+        if event == 'online_changed':
+            self.on_time = args[0]
+        elif event == 'offline_changed':
+            self.off_time = args[0]
+        elif event == 'stop':
+            self.stop_timer()
+        elif event == 'start':
+            self.start_timer()
+        elif event == 'restart':
+            self.stop_timer()
+            self.start_timer()
+        elif event == 'reset':
+            self.stop_timer()
+
+    def on_state_triggered(self, state=0):
+        self.rest_rtc()
+        self.stop_timer()
+        if state > self.STATE_STOPPED:
+            self.start_timer()
+        self.set_state(state)
+        self.led.value(max(0, state-1))
+
+
 class Controller(RObject):
     title = 'No Title'
     selectable = True
     max_width = 18
+
+    def __repr__(self):
+        return '<Ctrl {}>'.format(self.title)
 
     def __init__(self):
         super(Controller, self).__init__()
@@ -124,13 +246,14 @@ class Controller(RObject):
         self.render()
 
     def on_exit(self):
-        pass
+        self.render()
 
     def render(self):
         if self.line is not None:
             text = '{:<{}}'.format(self.get_title(), self.max_width)
             value = self.get_value()
-            text = text[:-len(value)] + value
+            if value:
+                text = text[:-len(value)] + value
             LCD.print_line(self.line, text, start_pos=1)
 
 
@@ -212,6 +335,10 @@ class Menu(RObject):
         for i in range(4):
             LCD.print_line(i, ' ', start_pos=Display.LINES-1)
 
+    def receive(self, event, *args):
+        if event == 'rerender':
+            self.render_menu()
+
     def apply_events(self):
         objects = self.items[:] + [self.core, self]
         for item in objects:
@@ -219,7 +346,6 @@ class Menu(RObject):
             if e:
                 while e:
                     ev = e.pop()
-                    print('EMIT', ev)
                     for c in objects:
                         c.receive(ev[0], *ev[1])
 
@@ -229,33 +355,33 @@ class Menu(RObject):
 class ControllerTitle(Controller):
     selectable = False
     title_list = {
-        -1: '======',
-        0: '=OFF=',
-        1: '=ON='
+        Program.STATE_STOPPED: '='*18,
+        Program.STATE_ONLINE:  '=ON==',
+        Program.STATE_OFFLINE: '=OFF=',
     }
 
     def __init__(self):
         super(ControllerTitle, self).__init__()
         self.eta = 0
-        self.mode = -1
+        self.mode = Program.STATE_STOPPED
 
     def get_title(self):
-        return self.title_list[self.mode]
+        # return self.title_list[self.mode]
+        return (str(self.mode)+self.title_list[self.mode])[:18]
 
     def get_value(self):
+        if self.mode == Program.STATE_STOPPED:
+            return ''
+        # TODO full line with hours
         return '00:00:{:02d}'.format(self.eta)
-
-    def on_right(self):
-        pass
-
-    def on_left(self):
-        pass
 
     def receive(self, event, *args):
         if event == 'update_eta':
+            print('SET ETA:', args[0], 'Mode:', self.mode)
             self.eta = args[0]
             self.render()
-        elif event == 'set_mode':
+        elif event == 'set_state':
+            print('SET TITLE STATE', args[0])
             self.mode = args[0]
             self.render()
 
@@ -275,12 +401,14 @@ class ControllerTime(Controller):
         return '{}:{}'.format("{:02d}".format(h), "{:02d}".format(m))
 
     def on_left(self):
-        self.time = max(0, self.time-1)
-
-    def on_right(self):
         self.time += 1
         if self.time >= 6000:
             self.time = 0
+        super(ControllerTime, self).on_right()
+
+    def on_right(self):
+        self.time = max(0, self.time-1)
+        super(ControllerTime, self).on_left()
 
     def receive(self, event, *args):
         if event == 'reset':
@@ -289,7 +417,6 @@ class ControllerTime(Controller):
     def on_exit(self):
         if self.event_name:
             self.emit(self.event_name, self.time)
-
 
     # def on_exit(self):
     #     # TODO: save to flash
@@ -308,13 +435,28 @@ class ControllerOffline(ControllerTime):
 
 class ControllerActions(Controller):
     title = 'Action'
-    actions = [['Start', 'Stop'], 'Restart', 'Reset', 'Reboot', '<-back']
+    MODE_ACTIVE = 1
+    MODE_INACTIVE = 0
+    null_action = '<- back'
+    actions_base = ['Reset', 'Reboot', null_action]
+    actions = {
+        0: ['Start']+actions_base,
+        1: ['Stop', 'Restart']+actions_base,
+    }
 
     def __init__(self):
         super(ControllerActions, self).__init__()
         self.current_index = 0
-        self.max_index = len(self.actions) - 1
-        self.mode = 0
+        self.max_index = 0
+        self.mode = self.MODE_INACTIVE
+
+    def set_mode(self, value):
+        self.mode = value
+        self.max_index = len(self.actions[value]) - 1
+        self.render()
+
+    def get_title(self):
+        return super(ControllerActions, self).get_title()+str(self.mode)
 
     def get_value(self):
         return self.current_action()
@@ -323,120 +465,37 @@ class ControllerActions(Controller):
         self.current_index -= 1
         if self.current_index < 0:
             self.current_index = self.max_index
+        super(ControllerActions, self).on_left()
 
     def on_right(self):
         self.current_index += 1
         if self.current_index > self.max_index:
             self.current_index = 0
+        super(ControllerActions, self).on_right()
 
     def current_action(self):
-        action = self.actions[self.current_index]
-        if isinstance(action, list):
-            action = action[self.mode]
-        return action
+        return self.actions[self.mode][self.current_index]
 
     def on_exit(self):
         action = self.current_action()
-        if action != self.actions[-1]:
-            self.emit(action.lower().replace(' ', '_'))
+        if action != self.null_action:
+            event = action.lower().replace(' ', '_')
+            self.emit(event)
         self.current_index = 0
+        super(ControllerActions, self).on_exit()
 
     def receive(self, event, *args):
-        if event == 'set_mode':
-            self.mode = args[0]
-
-
-class Program(RObject):
-    STATE_ONLINE = 1
-    STATE_OFFLINE = 0
-    STATE_OFF = -1
-
-    def __init__(self):
-        super(Program, self).__init__()
-        self.state = self.STATE_ONLINE
-        self.led = Pin(LED_PIN, Pin.OUT)
-        self.led.value(0)
-        self.rtc = RTC()
-        self.timer = None
-        self.on_time = 0
-        self.off_time = 0
-        self.eta = 0
-        self.emit('set_mode', self.state)
-
-    def start_timer(self):
-        # https://micronote.tech/2020/02/Timers-and-Interrupts-with-a-NodeMCU-and-MicroPython/
-        # check times
-        if self.timer:
-            self.stop_timer()
-        self.timer = Timer(-2)
-        self.timer.init(period=1000, mode=Timer.PERIODIC, callback=self.update_handler)
-        self.rest_rtc()
-
-    def rest_rtc(self):
-        self.rtc.datetime((2020, 1, 1, 0, 0, 0, 0, 0))
-
-    def stop_timer(self):
-        if self.timer:
-            self.timer.deinit()
-            self.eta = 0
-            self.timer = None
-
-    def _get_time_offset(self):
-        if not self.timer:
-            return 0
-        _, _, _, _, h, m, s, _ = self.rtc.datetime()
-        return (h * 60 * 60) + (m * 60) + s
-
-    def update_handler(self, *args):
-        # get current time offset
-        total_sec = self._get_time_offset()
-        # compute eta
-        # trigger state if need
-        if self.state == self.STATE_ONLINE:
-            self.eta = self.on_time - total_sec
-            if total_sec >= self.on_time:
-                self.on_state_triggered(False)
-        elif self.state == self.STATE_OFFLINE:
-            self.eta = self.off_time - total_sec
-            if total_sec >= self.off_time:
-                self.on_state_triggered(True)
-        self.update_display()
-
-    def update_display(self):
-        self.emit('update_eta', self.eta)
-
-    def receive(self, event, *args):
-        if event == 'online_changed':
-            self.on_time = args[0]
-        elif event == 'offline_changed':
-            self.off_time = args[0]
-        elif event == 'stop':
-            if self.state == self.STATE_OFFLINE:
-                return
-            self.stop_timer()
-        elif event == 'start':
-            if self.state == self.STATE_ONLINE:
-                return
-            self.start_timer()
-        elif event == 'restart':
-            self.stop_timer()
-            self.start_timer()
-        elif event == 'reset':
-            self.stop_timer()
-
-    def on_state_triggered(self, set_online=False):
-        print('SWITCH TRIGGERED ->>>')
-        self.state = self.STATE_ONLINE if set_online else self.STATE_OFFLINE
-        self.rest_rtc()
-        self.stop_timer()
-        self.start_timer()
-        self.led.value(self.state)
-        self.emit('set_mode', self.state)
+        if event == 'set_state':
+            if args[0] == Program.STATE_STOPPED:
+                self.set_mode(self.MODE_INACTIVE)
+            else:
+                self.set_mode(self.MODE_ACTIVE)
+            self.render()
 
 
 def main():
     items = [
-        ControllerTitle(), ControllerOnline(5), ControllerOffline(10), ControllerActions()
+        ControllerTitle(), ControllerOnline(3), ControllerOffline(5), ControllerActions()
     ]
     core = Program()
     menu = Menu(items, core)
