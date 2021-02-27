90e503f7571370f1453c2c056c895cf272c53eb6
paulwinex <paulwinex@gmail.com>
2021-02-27 18:59:52 +0300

    fix main.py

--- src/main.py
+++ src/main.py
@@ -1,8 +1,8 @@
-import time_rele
+import time_relay
 import time
 
 
-time_rele.LCD.show_message('Time Relay\n----------', title='', timeout=0)
+time_relay.LCD.show_message('Time Relay\n----------', title='', timeout=0)
 time.sleep(2)
-time_rele.LCD.clear()
-time_rele.main()
+time_relay.LCD.clear()
+time_relay.main()
